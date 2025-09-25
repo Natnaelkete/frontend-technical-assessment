e3f3a7b311b5546f28db0b86d2f5514e67283d6b
natnaelkete <ketemanatnael023@gmail.com>
2025-09-25 11:57:19 +0300

    complete drag and drop

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-// Your implementation code will go here
+import { DragDrop } from "./dragDrop.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dd = new DragDrop({
+    root: document,
+    itemSelector: ".draggable-item",
+    dropSelector: ".drop-zone",
+  });
+  dd.init();
+});
